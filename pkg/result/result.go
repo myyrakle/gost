a47a43be4e4768b804ffde55a820f257067a9525
myyrakle <sssang97@naver.com>
2023-09-15 02:04:00 +0900

    feat: UnwrapOr, UnwrapOrElse

--- pkg/result/result.go
+++ pkg/result/result.go
@@ -180,3 +180,22 @@ func (self Result[T]) OrElse(op func(error) Result[T]) Result[T] {
 		return self
 	}
 }
+
+// Returns the contained Ok value or a provided default.
+// Arguments passed to unwrap_or are eagerly evaluated; if you are passing the result of a function call, it is recommended to use unwrap_or_else, which is lazily evaluated.
+func (self Result[T]) UnwrapOr(defaultValue T) T {
+	if self.IsOk() {
+		return *self.ok
+	} else {
+		return defaultValue
+	}
+}
+
+// Returns the contained Ok value or computes it from a closure.
+func (self Result[T]) UnwrapOrElse(f func() T) T {
+	if self.IsOk() {
+		return *self.ok
+	} else {
+		return f()
+	}
+}
