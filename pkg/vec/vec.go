a330b757cdd5a4b2a2d362b8942e3637a4fae078
myyrakle <sssang97@naver.com>
2023-09-15 02:42:19 +0900

    impl: Reserve

--- pkg/vec/vec.go
+++ pkg/vec/vec.go
@@ -14,3 +14,15 @@ func New[T any]() Vec[T] {
 func WithCapacity[T any](capacity int) Vec[T] {
 	return Vec[T]{data: make([]T, 0, capacity)}
 }
+
+// Returns the total number of elements the vector can hold without reallocating.
+func (self Vec[T]) Capacity() int {
+	return cap(self.data)
+}
+
+// Reserves capacity for at least additional more elements to be inserted in the given Vec<T>. The collection may reserve more space to speculatively avoid frequent reallocations. After calling reserve, capacity will be greater than or equal to self.len() + additional. Does nothing if capacity is already sufficient.
+func (self *Vec[T]) Reserve(additional int) {
+	if self.Capacity() < len(self.data)+additional {
+		self.data = append(self.data, make([]T, additional)...)
+	}
+}
