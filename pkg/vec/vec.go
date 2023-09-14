78820c542992532b4a58b2db85660e04c2d6e630
myyrakle <sssang97@naver.com>
2023-09-15 02:41:24 +0900

    impl: New, WithCapacity

--- pkg/vec/vec.go
+++ pkg/vec/vec.go
@@ -3,3 +3,14 @@ package vec
 type Vec[T any] struct {
 	data []T
 }
+
+// Constructs a new, empty Vec<T>.
+// The vector will not allocate until elements are pushed onto it.
+func New[T any]() Vec[T] {
+	return Vec[T]{data: make([]T, 0)}
+}
+
+// Constructs a new, empty Vec<T> with at least the specified capacity.
+func WithCapacity[T any](capacity int) Vec[T] {
+	return Vec[T]{data: make([]T, 0, capacity)}
+}
