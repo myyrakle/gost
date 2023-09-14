1d56776f50624423f3b755855cbd4603624d36a7
myyrakle <sssang97@naver.com>
2023-09-15 01:52:54 +0900

    impl: is_err, is_ok

--- pkg/result/result.go
+++ pkg/result/result.go
@@ -1,6 +1,34 @@
 package result
 
-type Result[T any, Err error] struct {
+type Result[T any] struct {
 	ok  *T
-	err Err
+	err error
+}
+
+// Returns true if the result is Ok.
+func (self Result[T]) IsOk() bool {
+	return self.ok != nil
+}
+
+// Returns true if the result is Ok and the value inside of it matches a predicate.
+func (self Result[T]) IsOkAnd(predicate func(T) bool) bool {
+	if self.IsOk() {
+		return predicate(*self.ok)
+	} else {
+		return false
+	}
+}
+
+// Returns true if the result is Err.
+func (self Result[T]) IsErr() bool {
+	return self.err != nil
+}
+
+// Returns true if the result is Err and the value inside of it matches a predicate.
+func (self Result[T]) IsErrAnd(predicate func(error) bool) bool {
+	if self.IsErr() {
+		return predicate(self.err)
+	} else {
+		return false
+	}
 }
