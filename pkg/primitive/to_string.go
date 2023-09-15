efe5a3a3bfa8b31709921d2f17f5ea2bf1261e04
myyrakle <sssang97@naver.com>
2023-09-16 00:54:31 +0900

    fix: string to String

--- pkg/primitive/to_string.go
+++ pkg/primitive/to_string.go
@@ -2,74 +2,74 @@ package primitive
 
 import "strconv"
 
-func (self Int) ToString() string {
-	return strconv.Itoa(int(self))
+func (self Int) ToString() String {
+	return String(strconv.Itoa(int(self)))
 }
 
-func (self Int8) ToString() string {
-	return strconv.Itoa(int(self))
+func (self Int8) ToString() String {
+	return String(strconv.Itoa(int(self)))
 }
 
-func (self Int16) ToString() string {
-	return strconv.Itoa(int(self))
+func (self Int16) ToString() String {
+	return String(strconv.Itoa(int(self)))
 }
 
-func (self Int32) ToString() string {
-	return strconv.Itoa(int(self))
+func (self Int32) ToString() String {
+	return String(strconv.Itoa(int(self)))
 }
 
-func (self Int64) ToString() string {
-	return strconv.Itoa(int(self))
+func (self Int64) ToString() String {
+	return String(strconv.Itoa(int(self)))
 }
 
-func (self Uint) ToString() string {
-	return strconv.Itoa(int(self))
+func (self Uint) ToString() String {
+	return String(strconv.Itoa(int(self)))
 }
 
-func (self Uint8) ToString() string {
-	return strconv.Itoa(int(self))
+func (self Uint8) ToString() String {
+	return String(strconv.Itoa(int(self)))
 }
 
-func (self Uint16) ToString() string {
-	return strconv.Itoa(int(self))
+func (self Uint16) ToString() String {
+	return String(strconv.Itoa(int(self)))
 }
 
-func (self Uint32) ToString() string {
-	return strconv.Itoa(int(self))
+func (self Uint32) ToString() String {
+	return String(strconv.Itoa(int(self)))
 }
 
-func (self Uint64) ToString() string {
-	return strconv.Itoa(int(self))
+func (self Uint64) ToString() String {
+	return String(strconv.Itoa(int(self)))
 }
 
-func (self Float32) ToString() string {
-	return strconv.FormatFloat(float64(self), 'f', -1, 32)
+func (self Float32) ToString() String {
+	return String(strconv.FormatFloat(float64(self), 'f', -1, 32))
 }
 
-func (self Float64) ToString() string {
-	return strconv.FormatFloat(float64(self), 'f', -1, 64)
+func (self Float64) ToString() String {
+	return String(strconv.FormatFloat(float64(self), 'f', -1, 64))
 }
 
-func (self Byte) ToString() string {
-	return strconv.Itoa(int(self))
+func (self Byte) ToString() String {
+	return String(strconv.Itoa(int(self)))
 }
 
-func (self Rune) ToString() string {
-	return strconv.Itoa(int(self))
+func (self Rune) ToString() String {
+	return String(strconv.Itoa(int(self)))
 }
 
-func (self String) ToString() string {
-	return string(self)
+func (self String) ToString() String {
+	return String(self)
 }
 
-func (self Bool) ToString() string {
-	return strconv.FormatBool(bool(self))
+func (self Bool) ToString() String {
+	return String(strconv.FormatBool(bool(self)))
 }
 
-func (self Complex64) ToString() string {
-	return strconv.FormatComplex(complex128(self), 'f', -1, 64)
+func (self Complex64) ToString() String {
+	return String(strconv.FormatComplex(complex128(self), 'f', -1, 64))
 }
 
-func (self Complex128) ToString() string {
-	return strconv.FormatComplex(complex128(self), 'f', -1, 128)
+func (self Complex128) ToString() String {
+	return String(strconv.FormatComplex(complex128(self), 'f', -1, 128))
 }
