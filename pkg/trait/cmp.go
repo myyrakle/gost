7f953ae6d2abeee9b4cbc495d487e313f203bbdc
myyrakle <sssang97@naver.com>
2023-09-16 02:47:51 +0900

    add: Ord trait

--- pkg/trait/cmp.go
+++ pkg/trait/cmp.go
@@ -1,6 +1,12 @@
 package trait
 
+import "github.com/myyrakle/gost/pkg/primitive"
+
 // Ordering enum values
-const OrderingLess = -1
-const OrderingEqual = 0
-const OrderingGreater = 1
+const OrderingLess = primitive.Int(-1)
+const OrderingEqual = primitive.Int(0)
+const OrderingGreater = primitive.Int(1)
+
+type Ord[T any] interface {
+	Cmp(rhs T) primitive.Int
+}
