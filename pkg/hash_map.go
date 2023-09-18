f5223978e52000db217753bd3bbb88687c9f8ab8
myyrakle <sssang97@naver.com>
2023-09-18 23:19:57 +0900

    [#14] impl: Insert

--- pkg/hash_map.go
+++ pkg/hash_map.go
@@ -15,16 +15,28 @@ func HashMapWithCapacity[K comparable, V any](capacity Int) HashMap[K, V] {
 }
 
 // As Raw Map
-func (m HashMap[K, V]) AsMap() map[K]V {
-	return m.data
+func (self HashMap[K, V]) AsMap() map[K]V {
+	return self.data
 }
 
 // Returns the number of elements in the map.
-func (m HashMap[K, V]) Len() Int {
-	return Int(len(m.data))
+func (self HashMap[K, V]) Len() Int {
+	return Int(len(self.data))
 }
 
 // Returns true if the map contains no elements.
-func (m HashMap[K, V]) IsEmpty() Bool {
-	return m.Len() == 0
+func (self HashMap[K, V]) IsEmpty() Bool {
+	return self.Len() == 0
+}
+
+// Inserts a key-value pair into the map.
+// If the map did not have this key present, None is returned.
+func (self *HashMap[K, V]) Insert(key K, value V) Option[V] {
+	old, ok := self.data[key]
+	self.data[key] = value
+	if ok {
+		return Some(old)
+	} else {
+		return None[V]()
+	}
 }
