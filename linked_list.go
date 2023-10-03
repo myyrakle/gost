3887d28bf842bca3338157981d0664e1faaaa975
myyrakle <sssang97@naver.com>
2023-10-03 21:39:29 +0900

    [#16] feat: PushBack

--- linked_list.go
+++ linked_list.go
@@ -15,3 +15,25 @@ type LinkedListNode[T any] struct {
 func LinkedListNew[T any]() LinkedList[T] {
 	return LinkedList[T]{}
 }
+
+// Appends an element to the back of a list.
+// This operation should compute in O(1) time.
+func (list *LinkedList[T]) PushBack(value T) {
+	newNode := LinkedListNode[T]{
+		value: value,
+		next:  nil,
+		prev:  nil,
+	}
+
+	newNode.prev = list.tail
+
+	if list.tail != nil {
+		list.tail.next = &newNode
+	}
+
+	list.tail = &newNode
+
+	if list.head == nil {
+		list.head = &newNode
+	}
+}
