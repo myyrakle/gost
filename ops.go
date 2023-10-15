259540450c0a2c82431109986d417c1a5089ba5b
myyrakle <sssang97@naver.com>
2023-10-15 20:24:11 +0900

    [#12] impl Add for primitives

--- ops.go
+++ ops.go
@@ -44,3 +44,67 @@ type DivAssign[T any] interface {
 func (self ISize) Add(rhs ISize) ISize {
 	return self + rhs
 }
+
+func (self I8) Add(rhs I8) I8 {
+	return self + rhs
+}
+
+func (self I16) Add(rhs I16) I16 {
+	return self + rhs
+}
+
+func (self I32) Add(rhs I32) I32 {
+	return self + rhs
+}
+
+func (self I64) Add(rhs I64) I64 {
+	return self + rhs
+}
+
+func (self USize) Add(rhs USize) USize {
+	return self + rhs
+}
+
+func (self U8) Add(rhs U8) U8 {
+	return self + rhs
+}
+
+func (self U16) Add(rhs U16) U16 {
+	return self + rhs
+}
+
+func (self U32) Add(rhs U32) U32 {
+	return self + rhs
+}
+
+func (self U64) Add(rhs U64) U64 {
+	return self + rhs
+}
+
+func (self F32) Add(rhs F32) F32 {
+	return self + rhs
+}
+
+func (self F64) Add(rhs F64) F64 {
+	return self + rhs
+}
+
+func (self Byte) Add(rhs Byte) Byte {
+	return self + rhs
+}
+
+func (self Char) Add(rhs Char) Char {
+	return self + rhs
+}
+
+func (self String) Add(rhs String) String {
+	return self + rhs
+}
+
+func (self Complex64) Add(rhs Complex64) Complex64 {
+	return self + rhs
+}
+
+func (self Complex128) Add(rhs Complex128) Complex128 {
+	return self + rhs
+}
