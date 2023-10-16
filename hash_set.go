6a167e7e95dc4c68ba0112b258811db51b77f15c
myyrakle <sssang97@naver.com>
2023-10-17 00:47:48 +0900

    [#15] impl: HashSet::WithCapacity

--- hash_set.go
+++ hash_set.go
@@ -8,3 +8,8 @@ type HashSet[K comparable] struct {
 func HashSetNew[K comparable]() HashSet[K] {
 	return HashSet[K]{hashMap: HashMapNew[K, struct{}]()}
 }
+
+// Creates an empty HashSet with at least the specified capacity.
+func HashSetWithCapacity[K comparable](capacity ISize) HashSet[K] {
+	return HashSet[K]{hashMap: HashMapWithCapacity[K, struct{}](capacity)}
+}
