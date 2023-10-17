1e8ff1a844c4b8f6dacc0021eac8c93b405d994f
myyrakle <sssang97@naver.com>
2023-10-18 00:21:18 +0900

    [#18] add: BTreeMap::Insert

--- btree_map.go
+++ btree_map.go
@@ -19,10 +19,10 @@ type BTreeNodeRef[K Ord[K], V any] struct {
 	// constant of the node that cannot be entirely described by `Type`, and that
 	// the node itself does not store. We only need to store the height of the root
 	// node, and derive every other node's height from it.
-	// Must be zero if `Type` is `Leaf` and non-zero if `Type` is `Internal`.
+	// Must be zero if `_Type` is `Leaf` and non-zero if `Type` is `Internal`.
 	height uint
 
-	nodeType _NodeType
+	_Type _NodeType
 
 	node BTreeNode[K, V]
 }
@@ -38,4 +38,20 @@ type BTreeNode[K Ord[K], V any] struct {
 // Creates an empty BTreeMap.
 func BTreeMapNew[K Ord[K], V any]() BTreeMap[K, V] {
 	return BTreeMap[K, V]{}
+}
+
+// Inserts a key-value pair into the map.
+// If the map did not have this key present, None is returned.
+// If the map did have this key present, the value is updated, and the old value is returned. The key is not updated, though; this matters for types that can be == without being identical. See the module-level documentation for more.
+func (m *BTreeMap[K, V]) Insert(key K, value V) Option[V] {
+	if m.root == nil {
+		m.root = &BTreeNodeRef[K, V]{
+			_Type: _LEAF_OF_INTERNAL,
+			node: BTreeNode[K, V]{
+				keys:   make([]*K, _BTREE_CAPACITY),
+				values: make([]*V, _BTREE_CAPACITY),
+			},
+		}
+	}
+	return m.root.insert(key, value)
 }
