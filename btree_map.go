6a22241b4c1c4c5ad379f704af9c9cd398f1f0e2
myyrakle <sssang97@naver.com>
2023-10-18 00:13:16 +0900

    [#18] add cap constants, redefine structs

--- btree_map.go
+++ btree_map.go
@@ -1,14 +1,30 @@
 package gost
 
+const _B = 6
+const _BTREE_CAPACITY = _B*2 - 1
+
 type BTreeMap[K Ord[K], V any] struct {
 	root *BTreeNode[K, V]
+	len  uint
+}
+
+type BTreeNodeRef[K Ord[K], V any] struct {
+	/// The number of levels that the node and the level of leaves are apart, a
+	/// constant of the node that cannot be entirely described by `Type`, and that
+	/// the node itself does not store. We only need to store the height of the root
+	/// node, and derive every other node's height from it.
+	/// Must be zero if `Type` is `Leaf` and non-zero if `Type` is `Internal`.
+	height uint
+
+	node BTreeNode[K, V]
 }
 
 type BTreeNode[K Ord[K], V any] struct {
-	key   K
-	value V
-	left  *BTreeNode[K, V]
-	right *BTreeNode[K, V]
+	keys   []*K
+	values []*V
+
+	/// The number of keys and values this node stores.
+	len uint16
 }
 
 // Creates an empty BTreeMap.
