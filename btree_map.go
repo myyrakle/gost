b875e95154a6d45c3cc64a60289ad5ade5af08b9
myyrakle <sssang97@naver.com>
2023-10-18 00:17:32 +0900

    [#18] add NodeType

--- btree_map.go
+++ btree_map.go
@@ -3,18 +3,26 @@ package gost
 const _B = 6
 const _BTREE_CAPACITY = _B*2 - 1
 
+type _NodeType int8
+
+const _LEAF = _NodeType(0)
+const _INTERNAL = _NodeType(1)
+const _LEAF_OF_INTERNAL = _NodeType(2)
+
 type BTreeMap[K Ord[K], V any] struct {
-	root *BTreeNode[K, V]
+	root *BTreeNodeRef[K, V]
 	len  uint
 }
 
 type BTreeNodeRef[K Ord[K], V any] struct {
-	/// The number of levels that the node and the level of leaves are apart, a
-	/// constant of the node that cannot be entirely described by `Type`, and that
-	/// the node itself does not store. We only need to store the height of the root
-	/// node, and derive every other node's height from it.
-	/// Must be zero if `Type` is `Leaf` and non-zero if `Type` is `Internal`.
+	// The number of levels that the node and the level of leaves are apart, a
+	// constant of the node that cannot be entirely described by `Type`, and that
+	// the node itself does not store. We only need to store the height of the root
+	// node, and derive every other node's height from it.
+	// Must be zero if `Type` is `Leaf` and non-zero if `Type` is `Internal`.
 	height uint
+
+	nodeType _NodeType
 
 	node BTreeNode[K, V]
 }
@@ -23,7 +31,7 @@ type BTreeNode[K Ord[K], V any] struct {
 	keys   []*K
 	values []*V
 
-	/// The number of keys and values this node stores.
+	// The number of keys and values this node stores.
 	len uint16
 }
 
