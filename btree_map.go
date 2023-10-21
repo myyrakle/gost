361f50f8d468d7eb9eea9df0787992d6fb0e081a
myyrakle <sssang97@naver.com>
2023-10-21 21:33:33 +0900

    [#18] impl insert

--- btree_map.go
+++ btree_map.go
@@ -7,32 +7,23 @@ type _NodeType int8
 
 const _LEAF = _NodeType(0)
 const _INTERNAL = _NodeType(1)
-const _LEAF_OF_INTERNAL = _NodeType(2)
+const _ROOT = _NodeType(2)
 
 type BTreeMap[K Ord[K], V any] struct {
-	root *BTreeNodeRef[K, V]
+	root *BTreeNode[K, V]
 	len  uint
 }
 
-type BTreeNodeRef[K Ord[K], V any] struct {
-	// The number of levels that the node and the level of leaves are apart, a
-	// constant of the node that cannot be entirely described by `Type`, and that
-	// the node itself does not store. We only need to store the height of the root
-	// node, and derive every other node's height from it.
-	// Must be zero if `_Type` is `Leaf` and non-zero if `Type` is `Internal`.
-	height uint
-
-	_Type _NodeType
-
-	node BTreeNode[K, V]
-}
-
 type BTreeNode[K Ord[K], V any] struct {
-	keys   Vec[*K]
-	values Vec[*V]
+	_Type          _NodeType
+	_MinimumDegree int // Minimum degree (defines the range for number of keys)
 
-	// The number of keys and values this node stores.
-	len uint16
+	keys   Vec[K]
+	values Vec[V]
+
+	n int // Current number of keys
+
+	childs Vec[*BTreeNode[K, V]]
 }
 
 // Creates an empty BTreeMap.
@@ -44,30 +35,229 @@ func BTreeMapNew[K Ord[K], V any]() BTreeMap[K, V] {
 // If the map did not have this key present, None is returned.
 // If the map did have this key present, the value is updated, and the old value is returned. The key is not updated, though; this matters for types that can be == without being identical. See the module-level documentation for more.
 func (self *BTreeMap[K, V]) Insert(key K, value V) Option[V] {
+	// If tree is empty
 	if self.root == nil {
-		self.root = &BTreeNodeRef[K, V]{
-			_Type: _LEAF_OF_INTERNAL,
-			node: BTreeNode[K, V]{
-				keys:   VecWithLen[*K](_BTREE_CAPACITY),
-				values: VecWithLen[*V](_BTREE_CAPACITY),
-			},
+		// Allocate memory for root
+		self.root = &BTreeNode[K, V]{
+			_Type:  _LEAF,
+			keys:   VecWithLen[K](_BTREE_CAPACITY),
+			values: VecWithLen[V](_BTREE_CAPACITY),
+		}
+
+		self.root.keys.SetUnchecked(0, key)
+		self.root.values.SetUnchecked(0, value)
+		self.len = 1
+		return None[V]()
+	} else /* If tree is not empty */ {
+		// If exists, update value
+		// TODO: implement update
+
+		// If root is full, then tree grows in height
+		if self.root.n == _BTREE_CAPACITY {
+			// Allocate memory for new root
+			newRoot := &BTreeNode[K, V]{
+				_Type:          _INTERNAL,
+				_MinimumDegree: _BTREE_CAPACITY,
+			}
+
+			// Make old root as child of new root
+			newRoot.childs.SetUnchecked(0, self.root)
+
+			// Split the old root and move 1 key to the new root
+			newRoot.splitChild(0, self.root)
+
+			// New root has two children now.  Decide which of the
+			// two children is going to have new key
+			i := USize(0)
+			if newRoot.keys.GetUnchecked(0).Cmp(key) == OrderingLess {
+				i++
+			}
+			newRoot.childs.GetUnchecked(i)._InsertNonFull(key, value)
+
+			// Change root
+			self.root = newRoot
+			return None[V]()
+		} else /* If root is not full, call insertNonFull for root */ {
+			self.root._InsertNonFull(key, value)
+			return None[V]()
 		}
 	}
-	result := self.root.insert(key, value)
-	self.len++
-
-	return result
 }
 
-func (self *BTreeNodeRef[K, V]) insert(key K, value V) Option[V] {
-	if self._Type == _LEAF_OF_INTERNAL {
-		for i, _key := range self.node.keys.AsSlice() {
-			if _key == nil {
-				self.node.keys.SetUnchecked(USize(i), &key)
-				self.node.values.SetUnchecked(USize(i), &value)
-				self.node.len++
-				return None[V]()
+// Function to traverse all nodes in a subtree rooted with this node
+func (self BTreeNode[K, V]) _Traverse() {
+	// There are n keys and n+1 children, traverse through n keys
+	// and first n children
+
+	i := USize(0)
+	for i < self.keys.Len() {
+		// If this is not leaf, then before printing key[i],
+		// traverse the subtree rooted with child C[i].
+		if self._Type != _LEAF {
+			self.childs.GetUnchecked(i)._Traverse()
+		}
+		Println("{}", self.keys.GetUnchecked(i))
+		i++
+	}
+}
+
+// Function to search key k in subtree rooted with this node
+func (self BTreeNode[K, V]) _Search(key K) Option[V] {
+	// Find the first key greater than or equal to k
+	i := USize(0)
+	for i < self.keys.Len() && key.Cmp(self.keys.GetUnchecked(i)) == OrderingGreater {
+		i++
+	}
+
+	// If the found key is equal to k, return this node
+	if i < self.keys.Len() && key.Cmp(self.keys.GetUnchecked(i)) == OrderingEqual {
+		return Some[V](self.values.GetUnchecked(i))
+	}
+
+	// If key is not found here and this is a leaf node
+	if self._Type == _LEAF {
+		return None[V]()
+	}
+
+	// Go to the appropriate child
+	return self.childs.GetUnchecked(i)._Search(key)
+}
+
+// The main function that inserts a new key in this B-Tree
+func (self *BTreeMap[K, V]) _Insert(key K, value V) {
+	// If tree is empty
+	if self.root == nil {
+		// Allocate memory for root
+		self.root = &BTreeNode[K, V]{
+			_Type:  _LEAF,
+			keys:   VecWithCapacity[K](_BTREE_CAPACITY),
+			values: VecWithCapacity[V](_BTREE_CAPACITY),
+		}
+		self.root.keys.Push(key)
+		self.root.values.Push(value)
+		self.len = 1
+	} else { // If tree is not empty
+		// If root is full, then tree grows in height
+		if self.root.keys.Len() == _BTREE_CAPACITY {
+			// Allocate memory for new root
+			newRoot := &BTreeNode[K, V]{
+				_Type:  _INTERNAL,
+				keys:   VecWithCapacity[K](_BTREE_CAPACITY),
+				values: VecWithCapacity[V](_BTREE_CAPACITY),
 			}
+
+			// Make old root as child of new root
+			newRoot.childs.Push(self.root)
+
+			// Split the old root and move 1 key to the new root
+			newRoot.splitChild(0, self.root)
+
+			// New root has two children now.  Decide which of the
+			// two children is going to have new key
+			i := USize(0)
+			if newRoot.keys.GetUnchecked(0).Cmp(key) == OrderingLess {
+				i++
+			}
+			newRoot.childs.GetUnchecked(i)._InsertNonFull(key, value)
+
+			// Change root
+			self.root = newRoot
+		} else { // If root is not full, call insertNonFull for root
+			self.root._InsertNonFull(key, value)
 		}
 	}
+}
+
+// A utility function to insert a new key in this node
+// The assumption is, the node must be non-full when this
+// function is called
+func (self *BTreeNode[K, V]) _InsertNonFull(key K, value V) {
+	// Initialize index as index of rightmost element
+	i := self.keys.Len() - 1
+
+	// If this is a leaf node
+	if self._Type == _LEAF {
+		// The following loop does two things
+		// a) Finds the location of new key to be inserted
+		// b) Moves all greater keys to one place ahead
+		for i >= 0 && self.keys.GetUnchecked(i).Cmp(key) == OrderingGreater {
+			self.keys.SetUnchecked(USize(i+1), self.keys.GetUnchecked(i))
+			self.values.SetUnchecked(USize(i+1), self.values.GetUnchecked(i))
+			i--
+		}
+
+		// Insert the new key at found location
+		self.keys.SetUnchecked(USize(i+1), key)
+		self.values.SetUnchecked(USize(i+1), value)
+	} else { // If this node is not leaf
+		// Find the child which is going to have the new key
+		for i >= 0 && self.keys.GetUnchecked(i).Cmp(key) == OrderingGreater {
+			i--
+		}
+
+		// See if the found child is full
+		if self.childs.GetUnchecked(i+1).keys.Len() == _BTREE_CAPACITY {
+			// If the child is full, then split it
+			self.splitChild(i+1, self.childs.GetUnchecked(i+1))
+
+			// After split, the middle key of C[i] goes up and
+			// C[i] is splitted into two.  See which of the two
+			// is going to have the new key
+			if self.keys.GetUnchecked(i+1).Cmp(key) == OrderingLess {
+				i++
+			}
+		}
+		self.childs.GetUnchecked(i+1)._InsertNonFull(key, value)
+	}
+}
+
+// A utility function to split the child y of this node
+// Note that y must be full when this function is called
+func (self *BTreeNode[K, V]) splitChild(i USize, y *BTreeNode[K, V]) {
+	// Create a new node which is going to store (t-1) keys
+	// of y
+	z := &BTreeNode[K, V]{
+		_Type:          y._Type,
+		_MinimumDegree: y._MinimumDegree,
+	}
+	z.n = z._MinimumDegree - 1
+
+	// Copy the last (t-1) keys of y to z
+	for j := 0; j < z._MinimumDegree; j++ {
+		z.keys.SetUnchecked(USize(j), y.keys.GetUnchecked(USize(j+self._MinimumDegree)))
+		z.values.SetUnchecked(USize(j), y.values.GetUnchecked(USize(j+self._MinimumDegree)))
+	}
+
+	// Copy the last t children of y to z
+	if y._Type != _LEAF {
+		for j := 0; j < self._MinimumDegree; j++ {
+			z.childs.SetUnchecked(USize(j), y.childs.GetUnchecked(USize(j+self._MinimumDegree)))
+		}
+	}
+
+	// Reduce the number of keys in y
+	y.n = self._MinimumDegree - 1
+
+	// Since this node is going to have a new child,
+	// create space of new child
+	for j := self.keys.Len(); j >= i+1; j-- {
+		self.childs.SetUnchecked(USize(j+1), self.childs.GetUnchecked(j))
+	}
+
+	// Link the new child to this node
+	self.childs.SetUnchecked(i+1, z)
+
+	// A key of y will move to this node. Find location of
+	// new key and move all greater keys one space ahead
+	for j := self.keys.Len() - 1; j >= i; j-- {
+		self.keys.SetUnchecked(USize(j+1), self.keys.GetUnchecked(j))
+		self.values.SetUnchecked(USize(j+1), self.values.GetUnchecked(j))
+	}
+
+	// Copy the middle key of y to this node
+	self.keys.SetUnchecked(i, y.keys.GetUnchecked(USize(self._MinimumDegree-1)))
+	self.values.SetUnchecked(i, y.values.GetUnchecked(USize(self._MinimumDegree-1)))
+
+	// Increment count of keys in this node
+	self.n++
 }
