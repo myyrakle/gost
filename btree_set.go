4cf887a4df1ccce83676395b63e0a6fba8fa3f9d
myyrakle <sssang97@naver.com>
2023-10-22 01:46:45 +0900

    [#46] feat: BTreeSet::Clear

--- btree_set.go
+++ btree_set.go
@@ -1,10 +1,15 @@
 package gost
 
 type BTreeSet[K Ord[K]] struct {
-	_treemap *BTreeNode[K, struct{}]
+	_treemap *BTreeMap[K, struct{}]
 }
 
 // Creates an empty BTreeSet.
 func BTreeSetNew[K Ord[K]]() BTreeSet[K] {
 	return BTreeSet[K]{}
+}
+
+// Clears the set, removing all elements.
+func (set *BTreeSet[K]) Clear() {
+	set._treemap.Clear()
 }
