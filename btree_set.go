4422b5dca2d578393109e362ee8017ee6a2c17e7
myyrakle <sssang97@naver.com>
2023-10-22 01:49:07 +0900

    [#46] feat: BTreeSet::Insert

--- btree_set.go
+++ btree_set.go
@@ -10,12 +10,26 @@ func BTreeSetNew[K Ord[K]]() BTreeSet[K] {
 }
 
 // Clears the set, removing all elements.
-func (set *BTreeSet[K]) Clear() {
-	set._treemap.Clear()
+func (self *BTreeSet[K]) Clear() {
+	self._treemap.Clear()
 }
 
 // Returns true if the set contains an element equal to the value.
 // The value may be any borrowed form of the set’s element type, but the ordering on the borrowed form must match the ordering on the element type.
-func (set *BTreeSet[K]) Contains(key K) Bool {
-	return set._treemap.ContainsKey(key)
+func (self *BTreeSet[K]) Contains(key K) Bool {
+	return self._treemap.ContainsKey(key)
+}
+
+// Adds a value to the set.
+// Returns whether the value was newly inserted. That is:
+// If the set did not previously contain an equal value, true is returned.
+// If the set already contained an equal value, false is returned, and the entry is not updated.
+func (self *BTreeSet[K]) Insert(key K) Bool {
+	result := self._treemap.Insert(key, struct{}{})
+
+	if result.IsSome() {
+		return false
+	} else {
+		return true
+	}
 }
