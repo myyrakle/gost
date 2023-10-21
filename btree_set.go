6f67d58b75209cd01d9252fd4d2dea6b251965ca
myyrakle <sssang97@naver.com>
2023-10-22 01:47:40 +0900

    [#46] feat: BTreeSet::Contains

--- btree_set.go
+++ btree_set.go
@@ -13,3 +13,9 @@ func BTreeSetNew[K Ord[K]]() BTreeSet[K] {
 func (set *BTreeSet[K]) Clear() {
 	set._treemap.Clear()
 }
+
+// Returns true if the set contains an element equal to the value.
+// The value may be any borrowed form of the set’s element type, but the ordering on the borrowed form must match the ordering on the element type.
+func (set *BTreeSet[K]) Contains(key K) Bool {
+	return set._treemap.ContainsKey(key)
+}
