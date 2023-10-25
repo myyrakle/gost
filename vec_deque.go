5bc08556f51f98dfba2bd698d8292a1047fc9ad9
myyrakle <sssang97@naver.com>
2023-10-26 02:02:08 +0900

    [#44] add VecDequeWithCapacity

--- vec_deque.go
+++ vec_deque.go
@@ -1,7 +1,27 @@
 package gost
 
+const (
+	_VECDEQUE_INITIAL_CAPACITY uint = 7 // 2^3 - 1
+	_VECDEQUE_MINIMUM_CAPACITY uint = 1 // 2 - 1
+)
+
+// A double-ended queue implemented with a growable ring buffer.
 type VecDeque[T any] struct {
 	buffer []T
+	len    uint
 	head   uint
-	tail   uint
+}
+
+// Creates an empty VecDeque.
+func VecDequeNew[T any]() VecDeque[T] {
+	return VecDequeWithCapacity[T](_VECDEQUE_INITIAL_CAPACITY)
+}
+
+// Creates an empty VecDeque with at least the specified capacity.
+func VecDequeWithCapacity[T any](capacity uint) VecDeque[T] {
+	if capacity < _VECDEQUE_MINIMUM_CAPACITY {
+		capacity = _VECDEQUE_MINIMUM_CAPACITY
+	}
+
+	return VecDeque[T]{buffer: make([]T, capacity), len: 0, head: 0}
 }
