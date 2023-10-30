6cda1e1ebc2826cb22c11441f2f550e92c98f654
myyrakle <sssang97@naver.com>
2023-10-31 02:51:04 +0900

    [#80] impl: Assert

--- panic.go
+++ panic.go
@@ -4,3 +4,10 @@ package gost
 func Panic(message String, args ...any) {
 	panic(Format(message, args...))
 }
+
+// Asserts that a boolean expression is true at runtime.
+func Assert(condition Bool, message String, args ...any) {
+	if !condition {
+		panic(Format(message, args...))
+	}
+}
