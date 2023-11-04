a4741241ad6696868980eebfdeb11bbd8979540a
myyrakle <sssang97@naver.com>
2023-11-05 00:38:04 +0900

    [#92] docs

--- future.go
+++ future.go
@@ -5,14 +5,17 @@ type Poll[T any] struct {
 	value *T
 }
 
+// check if the value is ready
 func (self *Poll[T]) IsReady() Bool {
 	return self.value != nil
 }
 
+// check if the value is pending
 func (self *Poll[T]) IsPending() Bool {
 	return self.value == nil
 }
 
+// unwrap the value
 func (self *Poll[T]) Unwrap() T {
 	return *self.value
 }
