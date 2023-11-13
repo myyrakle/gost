be2c3987d6e03f3315c73675716778ee7e50a17f
myyrakle <sssang97@naver.com>
2023-11-14 01:44:50 +0900

    [#58] add: Test_BTreeSet_IsSubset

---
+++ btree_set_test.go
@@ -0,0 +1,17 @@
+package gost
+
+import "testing"
+
+func Test_BTreeSet_IsSubset(t *testing.T) {
+	set1 := BTreeSetNew[I32]()
+	set1.Insert(I32(1))
+	set1.Insert(I32(2))
+
+	set2 := BTreeSetNew[I32]()
+	set2.Insert(I32(1))
+	set2.Insert(I32(2))
+	set2.Insert(I32(3))
+
+	Assert(set1.IsSubset(set2))
+	Assert(!set2.IsSubset(set1))
+}
