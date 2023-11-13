97f73272c5b8f7741af4bc5acea4500dd2852fa6
myyrakle <sssang97@naver.com>
2023-11-14 01:48:35 +0900

    [#58] add: Test_BTreeSet_Intersection

--- btree_set_test.go
+++ btree_set_test.go
@@ -29,3 +29,23 @@ func Test_BTreeSet_IsSuperset(t *testing.T) {
 	Assert(set2.IsSuperset(set1))
 	Assert(!set1.IsSuperset(set2))
 }
+
+func Test_BTreeSet_Intersection(t *testing.T) {
+	set1 := BTreeSetNew[I32]()
+	set1.Insert(I32(1))
+	set1.Insert(I32(2))
+	set1.Insert(I32(5))
+
+	set2 := BTreeSetNew[I32]()
+	set2.Insert(I32(1))
+	set2.Insert(I32(2))
+	set2.Insert(I32(3))
+
+	intersection := set1.Intersection(set2)
+
+	Assert(intersection.Len() == 2)
+	Assert(intersection.Contains(I32(1)))
+	Assert(intersection.Contains(I32(2)))
+	Assert(!intersection.Contains(I32(3)))
+	Assert(!intersection.Contains(I32(5)))
+}
