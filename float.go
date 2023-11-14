de97419ad3354f4a5fa629ba0a7c7ba722dad0e3
myyrakle <sssang97@naver.com>
2023-11-14 23:42:18 +0900

    [#105] impl: Float::Ceil

--- float.go
+++ float.go
@@ -27,3 +27,25 @@ func (self F32) Floor() F32 {
 func (self F64) Floor() F64 {
 	return F64(math.Floor(float64(self)))
 }
+
+// Returns the smallest integer greater than or equal to self.
+//
+//  f := gost.F32(3.01)
+//  g := gost.F32(4.0)
+//
+//  gost.AssertEq(f.Ceil(), F32(4.0))
+//  gost.AssertEq(g.Ceil(), F32(4.0))
+func (self F32) Ceil() F32 {
+	return F32(math.Ceil(float64(self)))
+}
+
+// Returns the smallest integer greater than or equal to self.
+//
+//  f := gost.F64(3.01)
+//  g := gost.F64(4.0)
+//
+//  gost.AssertEq(f.Ceil(), F64(4.0))
+//  gost.AssertEq(g.Ceil(), F64(4.0))
+func (self F64) Ceil() F64 {
+	return F64(math.Ceil(float64(self)))
+}
