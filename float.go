2b8f61720e5ffcbf7b0ec8169b338772005f2070
myyrakle <sssang97@naver.com>
2023-11-14 23:40:49 +0900

    [#105] impl: F32::Floor

--- float.go
+++ float.go
@@ -4,6 +4,19 @@ import "math"
 
 // Returns the largest integer less than or equal to self.
 //
+//  f := gost.F32(3.7)
+//  g := gost.F32(3.0)
+//  h := gost.F32(-3.7)
+//
+//  gost.AssertEq(f.Floor(), F32(3.0))
+//  gost.AssertEq(g.Floor(), F32(3.0))
+//  gost.AssertEq(h.Floor(), F32(-4.0))
+func (self F32) Floor() F32 {
+	return F32(math.Floor(float64(self)))
+}
+
+// Returns the largest integer less than or equal to self.
+//
 //  f := gost.F64(3.7)
 //  g := gost.F64(3.0)
 //  h := gost.F64(-3.7)
