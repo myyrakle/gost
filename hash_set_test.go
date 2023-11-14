3df12139cb84779f098dcb1523aee0dcc66b8bfb
myyrakle <sssang97@naver.com>
2023-11-14 23:33:48 +0900

    [#72] add: Test_HashSet_Union

--- hash_set_test.go
+++ hash_set_test.go
@@ -74,3 +74,24 @@ func Test_HashSet_IsDisjoint(t *testing.T) {
 	Assert(!set1.IsDisjoint(set2))
 	Assert(!set2.IsDisjoint(set1))
 }
+
+func Test_HashSet_Union(t *testing.T) {
+	t.Parallel()
+
+	set1 := HashSetNew[I32]()
+	set1.Insert(I32(1))
+	set1.Insert(I32(2))
+	set1.Insert(I32(3))
+
+	set2 := HashSetNew[I32]()
+	set2.Insert(I32(3))
+	set2.Insert(I32(4))
+
+	union := set1.Union(set2)
+
+	Assert(union.Len() == 4)
+	Assert(union.Contains(I32(1)))
+	Assert(union.Contains(I32(2)))
+	Assert(union.Contains(I32(3)))
+	Assert(union.Contains(I32(4)))
+}
