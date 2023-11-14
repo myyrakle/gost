268413ab7d9bcbdd5d05e6778ffccb735cb4844f
myyrakle <sssang97@naver.com>
2023-11-14 23:39:18 +0900

    [#105] impl: F64::Floor

---
+++ float.go
@@ -0,0 +1,16 @@
+package gost
+
+import "math"
+
+// Returns the largest integer less than or equal to self.
+//
+//  f := gost.F64(3.7)
+//  g := gost.F64(3.0)
+//  h := gost.F64(-3.7)
+//
+//  gost.AssertEq(f.Floor(), F64(3.0))
+//  gost.AssertEq(g.Floor(), F64(3.0))
+//  gost.AssertEq(h.Floor(), F64(-4.0))
+func (self F64) Floor() F64 {
+	return F64(math.Floor(float64(self)))
+}
