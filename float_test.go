4c9112747641fd8c87c4a0e659369a4da09f6551
myyrakle <sssang97@naver.com>
2023-11-14 23:41:12 +0900

    [#105] add: Test_F32_Floor

--- float_test.go
+++ float_test.go
@@ -13,3 +13,15 @@ func Test_F64_Floor(t *testing.T) {
 	AssertEq(g.Floor(), F64(3.0))
 	AssertEq(h.Floor(), F64(-4.0))
 }
+
+func Test_F32_Floor(t *testing.T) {
+	t.Parallel()
+
+	f := F32(3.7)
+	g := F32(3.0)
+	h := F32(-3.7)
+
+	AssertEq(f.Floor(), F32(3.0))
+	AssertEq(g.Floor(), F32(3.0))
+	AssertEq(h.Floor(), F32(-4.0))
+}
