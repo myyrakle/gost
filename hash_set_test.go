ebf387684a9daf0c8974b72eaba9dbe6bc96d5be
myyrakle <sssang97@naver.com>
2023-11-14 23:30:41 +0900

    [#72] add: Test_HashSet_Intersection

--- hash_set_test.go
+++ hash_set_test.go
@@ -33,3 +33,25 @@ func Test_HashSet_IsSuperset(t *testing.T) {
 	Assert(set2.IsSuperset(set1))
 	Assert(!set1.IsSuperset(set2))
 }
+
+func Test_HashSet_Intersection(t *testing.T) {
+	t.Parallel()
+
+	set1 := HashSetNew[I32]()
+	set1.Insert(I32(1))
+	set1.Insert(I32(2))
+	set1.Insert(I32(5))
+
+	set2 := HashSetNew[I32]()
+	set2.Insert(I32(1))
+	set2.Insert(I32(2))
+	set2.Insert(I32(3))
+
+	intersection := set1.Intersection(set2)
+
+	Assert(intersection.Len() == 2)
+	Assert(intersection.Contains(I32(1)))
+	Assert(intersection.Contains(I32(2)))
+	Assert(!intersection.Contains(I32(3)))
+	Assert(!intersection.Contains(I32(5)))
+}
