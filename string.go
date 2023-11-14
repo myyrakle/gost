efaff259687e3075e7a15c12d37f37d43b3e258c
myyrakle <sssang97@naver.com>
2023-11-15 00:11:45 +0900

    [#109] impl: StringFromUTF16

--- string.go
+++ string.go
@@ -1,5 +1,22 @@
 package gost
 
-func StringFromUTF8Lossy(bytes []byte) String {
-	return String(bytes)
+import "unicode/utf16"
+
+// Converts a vector of bytes to a String.
+func StringFromUTF8(bytes Vec[U8]) String {
+	return String(bytes.data)
+}
+
+// Decode a UTF-16–encoded vector v into a String, returning Err if v contains any invalid data.
+func StringFromUTF16(bytes Vec[U16]) Result[String] {
+	data := bytes.data
+	convertedData := []uint16{}
+
+	for i := 0; i < len(data); i += 2 {
+		convertedData = append(convertedData, uint16(data[i]))
+	}
+
+	decoded := utf16.Decode(convertedData)
+
+	return Ok[String](String(decoded))
 }
