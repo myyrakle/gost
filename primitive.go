86f62d7340a8ee1de453060ee992f1e14413fe3e
myyrakle <sssang97@naver.com>
2023-11-20 23:04:44 +0900

    [#117] add: declare I128, U128

--- primitive.go
+++ primitive.go
@@ -8,12 +8,20 @@ type I8 int8
 type I16 int16
 type I32 int32
 type I64 int64
+type I128 struct {
+	hi I64
+	lo U64
+}
 
 type USize uint
 type U8 uint8
 type U16 uint16
 type U32 uint32
 type U64 uint64
+type U128 struct {
+	hi U64
+	lo U64
+}
 
 type F32 float32
 type F64 float64
