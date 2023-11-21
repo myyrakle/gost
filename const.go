6172c1b81f91ad251f1a4ee35081dc179c0e6186
myyrakle <sssang97@naver.com>
2023-11-22 00:12:59 +0900

    [#119] add: integer min/max constants

---
+++ const.go
@@ -0,0 +1,33 @@
+package gost
+
+import "math"
+
+const U8_MIN = U8(0)
+const U8_MAX = U8(math.MaxUint8)
+
+const U16_MIN = U16(0)
+const U16_MAX = U16(math.MaxUint16)
+
+const U32_MIN = U32(0)
+const U32_MAX = U32(math.MaxUint32)
+
+const U64_MIN = U64(0)
+const U64_MAX = U64(math.MaxUint64)
+
+const USize_MIN = USize(0)
+const USize_MAX = USize(math.MaxUint)
+
+const I8_MIN = I8(math.MinInt8)
+const I8_MAX = I8(math.MaxInt8)
+
+const I16_MIN = I16(math.MinInt16)
+const I16_MAX = I16(math.MaxInt16)
+
+const I32_MIN = I32(math.MinInt32)
+const I32_MAX = I32(math.MaxInt32)
+
+const I64_MIN = I64(math.MinInt64)
+const I64_MAX = I64(math.MaxInt64)
+
+const ISize_MIN = ISize(math.MinInt)
+const ISize_MAX = ISize(math.MaxInt)
