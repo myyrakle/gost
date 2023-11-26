8d485cf1673146553360b646d50fe16da5575590
myyrakle <sssang97@naver.com>
2023-11-26 22:20:31 +0900

    [#114] docs: some comments

--- thread.go
+++ thread.go
@@ -20,16 +20,19 @@ func Sleep(dur Duration) {
 	time.Sleep(duration)
 }
 
+// An owned permission to join on a thread (block on its termination).
 type JoinHandle struct {
 	channel chan Unit
 }
 
+// Waits for the associated thread to finish.
 func (self JoinHandle) Join() Result[Unit] {
 	<-self.channel
 
 	return Ok(Unit{})
 }
 
+// Spawns a new thread, returning a JoinHandle for it.
 func Spawn(f func()) JoinHandle {
 	channel := make(chan Unit)
 
